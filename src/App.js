16b7693ce4c0b8806997bf616e73857a693162c8
SpecTruNMaG <refugionet@gmail.com>
2022-03-16 09:58:48 -0500

    cambio de titulo principal

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Primera aplicacion publicada de React Miguel Angel</h1>
+          <h1>Sitio Web en construccion</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
